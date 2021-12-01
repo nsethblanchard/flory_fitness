c9147551ab9a4e62333934abff859c845c32702e
Seth Blanchard <nsethblanchard@gmail.com>
2021-12-01 08:18:19 -0500

    button.css done

--- src/components/Button.js
+++ src/components/Button.js
@@ -14,7 +14,13 @@ const Button = ({ children, type, onClick, buttonStyle, buttonSize }) => {
 
   return (
     <Link to="/sign-up" className={"btn-mobile"}>
-      <button>{children}</button>
+      <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
     </Link>
   );
 };
