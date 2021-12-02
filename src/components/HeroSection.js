f3b5a01c37f7bf47fba81c91da85339355326581
Seth Blanchard <nsethblanchard@gmail.com>
2021-12-02 08:14:44 -0500

    brought in pictures

---
+++ src/components/HeroSection.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Button from "./Button";
+import HeroSection from "./HeroSection.css";
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <video src="/videos/video-2.mp4" autoPlay loop muted />
+      <h1>Adventure Awaits</h1>
+      <p>What are you waiting for?</p>
+      <div className="hero-btns">
+        <Button
+          className="btns-"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Get Started
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
