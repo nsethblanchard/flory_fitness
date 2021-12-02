bf8302af4c371e23657c4c57066987c95c8e54f0
Seth Blanchard <nsethblanchard@gmail.com>
2021-12-02 11:46:21 -0500

    Card and cardItem info

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,11 +2,13 @@ import "../../App.css";
 import HeroSection from "../HeroSection";
 
 import React from "react";
+import Cards from "../Cards";
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <Cards />
     </>
   );
 }
