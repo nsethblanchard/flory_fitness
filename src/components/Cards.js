f3d8af400df1d8cb2fa50bdebcf0d346034b67b2
Seth Blanchard <nsethblanchard@gmail.com>
2021-12-03 08:40:47 -0500

    dummy cards at bottom of home screen

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,32 @@
 import React from "react";
 import CardItem from "./CardItem";
+import "./Cards.css";
 
 function Cards() {
   return (
     <div className="cards">
-      <h1>Check out the epic destinations!</h1>
+      <h1>Some examples of Services</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            <CardItem />
+            <CardItem
+              src="images/Christine Professional Pictures 2021/030A2746 plank.jpg"
+              text="Planking is fun times"
+              label="exercise"
+              path="/services"
+            />
+            <CardItem
+              src="images/Christine Professional Pictures 2021/Christine-19reverse warrior.jpg"
+              text="Yoga under the tracks"
+              label="yoga"
+              path="/services"
+            />
+            <CardItem
+              src="images/Christine Professional Pictures 2021/Christine-1 wall sports bra .jpg"
+              text="Standing near a wall is also fun times"
+              label="dry-stack stone wall"
+              path="/services"
+            />
           </ul>
         </div>
       </div>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,7 @@
-import "../../App.css";
-import HeroSection from "../HeroSection";
-
 import React from "react";
+import "../../App.css";
+
+import HeroSection from "../HeroSection";
 import Cards from "../Cards";
 
 function Home() {
